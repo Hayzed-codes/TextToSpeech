b20a3fda254965b0ed4e7fd941c6286d5067fdd1
Hayzed-codes <raheemhorlah@gmail.com>
2024-03-24 17:19:04 +0100

    update

--- script.js
+++ script.js
@@ -1,39 +1,56 @@
 const textArea = document.querySelector("textarea");
 const button = document.querySelector("button");
+const select = document.querySelector("select");
+// let voices = [];
 let isSpeaking = true;
+let speech = window.speechSynthesis;
 
 const textToSpeech = () => {
-  const speech = window.speechSynthesis;
   const text = textArea.value;
 
   if (!speech.speaking && text) {
     const utterSpeech = new SpeechSynthesisUtterance(text);
-    speech.speak(utterSpeech);
-  }
-
-
-  if (text.length > 50){
-    if(speech.speaking && isSpeaking) {
-        button.innerText = "Pause";
-        speech.resume();
-        isSpeaking = false;
-    } else {
-        button.innerText = "Resume";
-        speech.pause();
-        isSpeaking = true;
+    for (let voice of speech.getVoices()) {
+        if(voice.name === select.value) {
+            utterSpeech.voice = voice;
+        }
     }
-
+    speech.speak(utterSpeech);
   } else {
     isSpeaking = false;
-    button.innerText = "Speaking"
+  }
+
+  if (text.length > 50) {
+    if (speech.speaking && isSpeaking) {
+      button.innerText = "Pause";
+      speech.resume();
+      isSpeaking = false;
+    } else {
+      button.innerText = "Resume";
+      speech.pause();
+      isSpeaking = true;
+    }
+  } else {
+    isSpeaking = false;
+    button.innerText = "Speaking";
   }
 
   setInterval(() => {
-    if(!speech.speaking && !isSpeaking){ 
-        isSpeaking = true;
-        button.innerText = "Speak";
+    if (!speech.speaking && !isSpeaking) {
+      isSpeaking = true;
+      button.innerText = "Speak";
     }
-  })
+  });
 };
 
 button.addEventListener("Click", textToSpeech());
+
+const voices = () => {
+  for (let voice of speech.getVoices()) {
+      console.log(voice);
+    // Creating an option tag with passing voice name and voice language
+    let option = `<option value="${voice.name}"> ${voice.name} (${voice.lang})</option>`
+    select.insertAdjacentHTML ('beforeend', option) // Inserting option tag beforeend option
+  }
+};
+select.addEventListener("click", voices());
